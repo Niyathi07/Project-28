3cbb28aa8aa1a909fa4d4dccc2c168280cee255a
Niyathi07 <71333642+Niyathi07@users.noreply.github.com>
2020-11-02 20:12:10 +0530

    Add files via upload

---
+++ tree.js
@@ -0,0 +1,25 @@
+class Tree{
+    constructor(x, y, width , height) {
+      var options = {
+        isStatic:true
+      }
+      this.image = loadImage("tree.png");
+      this.body = Bodies.rectangle(x, y, width , height, options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+    }
+    display(){
+    var pos = this.body.position;
+
+        push ()
+        translate(this.body.position.x , this.body.position.y)
+
+        imageMode(CENTER);
+        image(this.image,0,0,this.r,this.r);
+        
+        
+        pop ()
+    }
+  };
+  
